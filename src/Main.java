4c12ac9f5673bba196117f29843b00e138f19e0b
grebenirina <grebenira64@gmail.com>
2024-09-10 21:46:26 +0300

    new branch commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello uhyuy!");
         System.out.println("another commit");
+        System.out.println("new branch commit");
     }
 }
